a7ca1c8187ff3fb9d029f565a2e3d20ba3ddb53a
benjithorpe <benthorpe.dev@gmail.com>
2022-10-09 07:49:17 +0000

    feat: add student model

---
+++ models/Student.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const studentSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 4,
+    max: 200,
+  },
+  username: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 20,
+  },
+  image: {
+    type: String,
+    default: 'default.png',
+  },
+  email: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 255,
+  },
+  institution: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+    default: 'Simple is better than complex',
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 1024,
+  },
+  joined: {
+    type: Date,
+    default: Date.now,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  gists: [mongoose.Types.ObjectId],
+});
+
+module.exports = mongoose.model('Student', studentSchema);

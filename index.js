9de20fd3ed54c2bc76861eab4af29bd5f5242b68
benjithorpe <benthorpe.dev@gmail.com>
2022-10-09 07:49:59 +0000

    feat: add utility

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const studentRoutes = require('./routes/students.js');
+const authRoutes = require('./routes/auth.js');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
+// Connect to DB
+mongoose.connect(process.env.DATABASE_URL, () => console.log('Connected...'));
+
+// Middlewares
+app.use(express.json());
+app.use(cors({ origin: '*' }));
+
+// Routes Middlewares
+app.use('/api/auth/', authRoutes);
+app.use('/api/students', studentRoutes);
+
+// Normal/Documentation Routes
 app.get('/', (req, res) => res.json({ message: 'hello world!' }));
 
-app.listen(5000);
+app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));

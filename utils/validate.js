a6d6ac085b2159cf4018760c656e05872aca1c81
benjithorpe <benthorpe.dev@gmail.com>
2022-10-10 09:05:50 +0000

    feat: add validation for login

--- utils/validate.js
+++ utils/validate.js
@@ -1,14 +1,11 @@
 import Joi from 'joi';
 
-// export const loginValidation = Joi.object({
-//   email: Joi.string().email({
-//     allowFullyQualified: true,
-//     tlds: { allow: ['com', 'net'] },
-//   }),
-//   password: Joi.string().min(4),
-// });
+export const loginValidation = Joi.object({
+  email: Joi.string().email().min(6).max(255),
+  password: Joi.string().min(6).max(1024),
+});
 
-export const registerStudentValidation = Joi.object({
+export const registerValidation = Joi.object({
   name: Joi.string().min(4).max(200).required(),
   username: Joi.string().min(2).max(20).required(),
   email: Joi.string().email().min(6).max(255).required(),
